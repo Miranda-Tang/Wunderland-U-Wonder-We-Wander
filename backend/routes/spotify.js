fc0bf67166e570f2382387b9acbb3a00221ef3a3
Miranda-Tang <mirandatang0708@gmail.com>
2024-01-20 20:17:47 -0800

    Add Spotify authentication and update dependencies
    Spotify login and callback routes have been added to the application, allowing users to authenticate Spotify accounts in the backend. Axios has also been added as a dependency in package.json and package-lock.json. The file structure has been updated to include these new routes.

---
+++ backend/routes/spotify.js
@@ -0,0 +1,48 @@
+import express from "express";
+import axios from "axios";
+
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+    const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+    const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+    const scopes = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + CLIENT_ID +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' + encodeURIComponent(REDIRECT_URI));
+});
+
+router.get('/callback', async (req, res) => {
+    const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+    const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+    const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+    if (req.query.error) {
+        return res.send('Error occurred while logging in');
+    }
+    const authorizationCode = req.query.code;
+    try {
+        const response = await axios({
+            url: 'https://accounts.spotify.com/api/token',
+            method: 'post',
+            params: {
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                redirect_uri: REDIRECT_URI,
+                grant_type: 'authorization_code',
+                code: authorizationCode,
+            },
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        });
+        const {access_token, refresh_token} = response.data;
+        res.json({access_token, refresh_token});
+    } catch (error) {
+        console.log(error);
+        return res.send('Error occurred while logging in');
+    }
+});
+
+export default router;
