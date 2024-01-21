8133e3dd3c23ac1b9a045db815c39940261c3c6e
Miranda-Tang <mirandatang0708@gmail.com>
2024-01-20 17:12:47 -0800

    Refactor file structure and update weather route path
    The weather.js file has been moved to a new 'routes' directory to improve code organization. Accordingly, the import statement of weatherRoutes in the server.js file has been updated to reflect this new path.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import weatherRoutes from "./weather.js";
+import weatherRoutes from "./routes/weather.js";
 
 dotenv.config();
 
